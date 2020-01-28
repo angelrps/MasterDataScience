f6ea09ebaea9d16b610f96524ce5f529e83199b8
Angel Ruiz-Peinado <angelruizpeinado@gmail.com>
2020-01-28 16:33:03 +0100

    My R exercicesç

---
+++ ExtraClassExercises/20200127_R Exercises.R
@@ -0,0 +1,68 @@
+# Genera un vector que reproduzca las cartas repartidas a un jugador de mus en una mano jugando a la baraja española.
+# Recuerda que hay cuatro palos: bastos, oros, copas y espadas. ¿Qué tipo de vector es? ¿Con que funcion lo conﬁrmamos?
+
+mano <- c('as_oros', "as_copas", "rey_expadas", "rey_bastos")
+class(mano)
+
+
+moneda <- list("cruz"=1, "cara"=0)
+attributes(moneda)
+names(moneda)
+
+p<-data.frame(figura=c("sota","caballo","rey"),puntos=c(10,11,12))
+p
+ 
+
+# Ejercicio 4: Construye una baraja española de cartas La baraja española tiene cuatro palos y 10 cartas por palo.
+# Cada una de ellas tiene una puntuación: As (11 puntos), Tres (10 puntos), Rey (4 puntos), Caballo (3 puntos) y Sota (2 puntos).
+# El resto de cartas no tienen valor puntuable.
+
+# 4.1. Construye una lista que contenga todas las posibles cartas que hay en la baraja y sus puntuaciones
+# 4.2. Construye un data frame donde cada ﬁla represente una combinacion de carta/palo/puntuacion 
+# 4.3. Cuantos puntos suma en total la baraja española?
+
+carta <- c('as', 2:7, 'sota', 'caballo', 'rey')
+palos <- c('oros', 'copas', 'espadas', 'bastos')
+puntos <- c(11, 0, 10, rep(0,4), 2, 3, 4)
+df_lista <- list(rep(carta,4), rep(palos, each=10), rep(puntos,4))
+
+df_baraja <- data.frame(carta=df_lista[[1]], palo=df_lista[[2]], puntos=df_lista[[3]])
+
+sum(df_baraja$puntos)
+df_baraja
+df_baraja[,1]
+df_baraja$carta
+
+
+# Ejercicio 5: Baraja y reparte 
+# 5.1. Construye una función deal<-function(){return()} que seleccione la primera carta de tu baraja 
+deal <- function(b){
+  return (b[1,])
+}
+deal(df_baraja)
+
+# 5.2. Baraja de manera que queden ordenadas de otra forma. 
+# Genera una función llamada shuﬄe que pueda ser utilizada para esto más veces.
+suffle <- function(b){
+  return(b[sample(1:40, 40),])
+}
+t <- suffle(df_baraja)
+t
+
+# 5.3. Selecciona la primera carta de esta baraja revuelta
+deal(suffle(df_baraja))
+
+
+# Ejercicio 6: ¿Cuántos ases hay en mi baraja? 
+# 6.1. Selecciona la columna carta del objeto baraja que contiene los nombres de las cartas
+# 6.2. Usa el operador lógico correspondiente para identiﬁcar todos los que tengan como nombre “as”. 
+# (Hint: recuerda que los caracteres se escriben entre comillas) 
+# 6.3. Crea un nuevo objeto baraja2 en el que pongas a 0 la puntuacion de los ases 
+
+df_baraja2 <- df_baraja
+df_baraja2[df_baraja2$carta == 'as','puntos'] <- 0
+
+df_baraja2
+
+
+
